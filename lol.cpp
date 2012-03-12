7d8857da326b57a07232b89c4fa336f9e8e48d26
Robin Singh <robin.singh.1991@gmail.com>
2012-03-12 00:03:26 -0400

    works

--- lol.cpp
+++ lol.cpp
@@ -6,6 +6,7 @@
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
 #include <boost/thread.hpp>
+#include <boost/timer/timer.hpp>
 #include <iostream>
 #include <stdlib.h>
 #include <stdio.h>
@@ -13,6 +14,8 @@
 #include <ctime>
 
 using namespace std;
+
+
 
 void initial(int &height, int &width);
 double timer(bool on_or_off);
@@ -27,7 +30,8 @@ int main (int argc, char ** argv)
 {
     int a;
     bool start=true, stop=false;
-    int * matrix;
+    int * matrix_dx;
+    int * matrix_dy;
     int * dx_matrix;
     int * dy_matrix;
     
@@ -35,27 +39,27 @@ int main (int argc, char ** argv)
     width = 0;
     initial(height, width);
     
-    matrix = new int [height*width];
-    dx_matrix = new int [height*width];
+    matrix_dx = new int [height*width];
+    matrix_dy = new int [height*width];
+		dx_matrix = new int [height*width];
     dy_matrix = new int [height*width];
     
-    delete[] matrix;
-    delete[] dx_matrix;
-    delete[] dy_matrix;
-
     cout << "fill with random data \n";
     //srand((unsigned)time(0)); 
-    for(a=0;a<height*width;a++)
-            matrix[a] = rand() % 10;
-    
-    
-    timer(start);
+    for(a=0;a<height*width;a++){
+            matrix_dx[a] = rand() % 10;
+    				matrix_dy[a] = matrix_dx[a];
+    }
+
+//    timer(start);
         cout << "kernel filter\n";
-        boost::thread workerone(kernel_filter_dx, matrix, dx_matrix, true);
-        boost::thread workertwo(kernel_filter_dy, matrix, dx_matrix, true);
+    		boost::thread workerone(kernel_filter_dx ,matrix_dx, dx_matrix, true);
+        boost::thread workertwo(kernel_filter_dy ,matrix_dy, dy_matrix, false);
+            //kernel_filter_dx(matrix, dx_matrix, true);    //dx_matrix
+            //kernel_filter_dy(matrix, dy_matrix, false);   //dy_matrix
         workerone.join();
         workertwo.join();
-    cout << "This is the time taken=" << timer(stop) << " seconds" << endl;
+//    cout << "This is the time taken=" << timer(stop) << " seconds" << endl;
 
     
     timer(start);
@@ -67,7 +71,11 @@ int main (int argc, char ** argv)
         //workerone.join();
         //workertwo.join();
     cout << "This is the time taken=" << timer(stop) << " seconds" << endl;
-
+		
+		delete[] matrix_dx;
+    delete[] matrix_dy;
+    delete[] dx_matrix;
+    delete[] dy_matrix;
     return 0;
 }
 
@@ -109,24 +117,25 @@ double timer(bool on){
         startClock = clock();
     } else {
         tempClock = clock();
-        timeCount = (tempClock-startClock)*100000/CLOCKS_PER_SEC;
-        timeCount = timeCount/100000;
+        timeCount = ((tempClock-startClock)*1)/CLOCKS_PER_SEC;
+        timeCount = timeCount/1;
     }
     return timeCount;
 }
 
 void kernel_filter_dx(int matrix[], int transform_matrix[], bool dx_matrix){
-    int a, b, c, x1, x2, x3;
+    boost::timer::auto_cpu_timer t("%t sec CPU, %w sec real");
+		int a, b, c, x1, x2, x3;
     char kernel[3] = {-1, 0, 1};
-    int value_array[height*width];
-    
-        c=0;
+    int * value_array;
+		value_array = new int [height*width];
+		    c=0;
         b=0;
         for (a=0;a<height*width; a++){
                 x1 = kernel[0] * matrix[a-1];
                 x2 = kernel[1] * matrix[a];
                 x3 = kernel[2] * matrix[a+1];
-
+								
                 value_array[b] = a;
                 b++;
                 if (a == (width*c)){
@@ -136,16 +145,22 @@ void kernel_filter_dx(int matrix[], int transform_matrix[], bool dx_matrix){
                 else if (a == width*c-1){
                     x3 = 0;
                 }
-            
+            		
             transform_matrix[a] = x1 + x2 + x3;
         }
-    } 
-    
+		delete[] value_array;
+} 
+
+//10 gb of memory with 1 gb of ram
+//Due to the kernel filter transpose matrix 
+
 void kernel_filter_dy(int matrix[], int transform_matrix[], bool dx_matrix){
-    int d, e, f, y1, y2, y3;
+    boost::timer::auto_cpu_timer t("%t sec CPU, %w sec real");
+		int d, e, f, y1, y2, y3;
     char kernel[3] = {-1, 0, 1};
-    int value_array[height*width];
-        e = 0;
+    int * value_array;
+		value_array = new int [height*width];
+		  e = 0;
         f = 0;
         for (d=0;d<height*width; d++){
                 y1 = kernel[0] * matrix[d-width];
@@ -161,14 +176,16 @@ void kernel_filter_dy(int matrix[], int transform_matrix[], bool dx_matrix){
                 value_array[e] = matrix[d];
                 transform_matrix[d] = y1 + y2 + y3;
         }
+		delete[] value_array;
     }
 
 
 void compute_min_and_max(int matrix[]){
-    int max_val1;
+    boost::timer::auto_cpu_timer t("%t sec CPU, %w sec real");
+		int max_val1;
     int min_val1;
     int a;
-    min_val1 = matrix[0];
+		min_val1 = matrix[0];
     max_val1 = matrix[0];
     for (a=0;a<width*height; a++){
             if (matrix[a] < min_val1)
